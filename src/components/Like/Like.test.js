c7429ba94f9c90427cf3b2e08964d3022a2e41bb
Xumeparu <nyaluxey@gmail.com>
2021-06-10 11:16:40 +0700

    Fixed test for Like component

--- src/components/Like/Like.test.js
+++ src/components/Like/Like.test.js
@@ -1,10 +1,32 @@
-import { render, screen } from '@testing-library/react';
+import {act, fireEvent, render, screen} from '@testing-library/react';
 import Like from './Like';
 
 test('Тестирование компонента Heart', () => {
     render(<Like />);
 
     const heart = screen.getByTestId('heart');
-
     expect(heart).toBeInTheDocument();
+    expect(heart.className).toBe('heart black');
+
+    jest.useFakeTimers();
+
+    fireEvent.click(heart);
+    act(() => {
+        jest.advanceTimersByTime(800);
+    });
+    fireEvent.click(heart);
+    expect(heart.className).toBe('heart black');
+
+    act(() => {
+        jest.clearAllTimers();
+    });
+
+    fireEvent.click(heart);
+    act(() => {
+        jest.advanceTimersByTime(799);
+    });
+    fireEvent.click(heart);
+    expect(heart.className).toBe('heart red');
+
+    jest.useRealTimers();
 });
